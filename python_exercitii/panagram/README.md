be8fe05716ca48948894313903f0f175a3a5e6e4
Stefan Caraiman <scaraiman@cloudbasesolutions.com>
2016-03-20 22:30:39 +0200

    Am adaugat exercitiul panagram.

---
+++ python_exercitii/panagram/README.md
@@ -0,0 +1,4 @@
+# Implement a panagram function
+A pangram is a sentence that contains all the letters of the English alphabet at least once.
+Ex: The quick brown fox jumps over the lazy dog. 
+Your task here is to write a function to check a sentence to see if it is a pangram or not.
---
+++ python_exercitii/panagram/panagram.py
@@ -0,0 +1,10 @@
+"""Checks whether a given string is a panagram."""
+
+def ispanagram(s):
+	pass
+
+if __name__ == "__main__":
+	pgram = "Go, lazy fat vixen; be shrewd, jump quick."
+	nprgram = "Trump is going to win the elections."
+	assert ispanagram(pgram) == True
+	assert ispanagram(nprgram) == False
